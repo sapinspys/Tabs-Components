dab17d9f314912ac6b87c7e8a959bc10068383c6
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-01-31 01:02:59 -0500

    Added tab JS functionality!

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -10,7 +10,7 @@ class Dropdown {
     this.content = this.element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', this.toggleContent.bind(this))
+    this.button.addEventListener('click', this.toggleContent.bind(this));
   };
 
   toggleContent() {
